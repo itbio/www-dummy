a48cb527d832f80b2dd23fdeefe08cc4b84230cd
Steve Strassmann <steve@strassmann.com>
2017-10-25 00:00:25 +0800

    fix texmap

--- texmap.py
+++ texmap.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import math
 import random
 
 COLORS = {'A': 'red',
-          'T': 'yellow',
-          'G': 'green',
-          'C': 'blue',
+          'T': '#FAA619', #'yellow',
+          'G': '#94BA33', #'green',
+          'C': '#01BADE', #'blue',
           '0': 'black',
           '1': 'gray'
           }
 
 def main():
     width, height = 1024, 1024
+    dx, dy = 64, 64
+    maxx, maxy = math.floor(width/dx), math.floor(height/dy)
     fontname = "SourceSansPro-Regular.otf"
-    fontsize = 48
+    fontsize = 62
     font = ImageFont.truetype(fontname, fontsize)
     img = Image.new('RGBA', (width, height))
     d = ImageDraw.Draw(img)
     
     char_set = 'ATGC10'
 
-    for j in range(0,25):
-        y = j * 50
-        for i in range(0, 25):
-            x = i * 50
+    for j in range(0, maxy):
+        y = j * dy
+        for i in range(0, maxx):
+            x = i * dx
             char = random.choice(char_set)
             color = COLORS[char]
-            d.text((x, y), char, fill=color, font=font)
+            w,h = font.getsize(char)
+            xoffset = (dx-w)/2
+            d.text((x+xoffset, y), char, fill=color, font=font)
     img.save("texture.png")
 
 
