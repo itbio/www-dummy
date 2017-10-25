b42867dc281acec7b9d5d53798763c7fe1aae97e
Steve Strassmann <steve@strassmann.com>
2017-10-26 02:07:33 +0800

    center placement

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ $(document).ready(function(){
     animate();
 });
 
+SIZE = 20;
+
 function init(){
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 70,
-					  window.innerWidth / window.innerHeight,
-					  1, 1000 );
+    camera = new THREE.PerspectiveCamera( 70, 1, 1, 1000 );
+    // Convert camera fov degrees to radians
+    var fov = camera.fov * ( Math.PI / 180 ); 
+
+    // Calculate the camera distance
+    var distance = Math.abs( SIZE / Math.sin( fov / 2 ) );
 
     var light = new THREE.DirectionalLight( 0xffffff );
     light.position.set( 0, 1, 1 ).normalize();
     scene.add(light);
 
-    var geometry = new THREE.SphereGeometry(20, 32, 32);
+    var geometry = new THREE.SphereGeometry(SIZE, 32, 32);
     var material = new THREE.MeshPhongMaterial(
 	{
 	    color:0xFFFFFF,  // 0x3366ff
@@ -26,18 +31,18 @@ function init(){
 	});
   
     mesh = new THREE.Mesh(geometry, material );
-    mesh.position.z = -100;
-    mesh.position.y = -6;
+    mesh.position.z = -40;
+    mesh.position.y = -0;
     //mesh.rotation.z += 0.35;
     scene.add( mesh );
   
     renderer = new THREE.WebGLRenderer({alpha: true});
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setClearColor( 0xffffff, 0);
-    document.body.appendChild( renderer.domElement );
+    $('.globe').append( renderer.domElement );
   
     window.addEventListener( 'resize', onWindowResize, false );
-  
+    resizeCamera();  
     render();
 }
 
@@ -53,10 +58,16 @@ function render(){
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    resizeCamera();
     render();
+}
+
+function resizeCamera() {    
+    var h = $('.title').height() * 1.5;
+    var w = h;
+    camera.aspect = w/h;
+    renderer.setSize(w, h)
+    camera.updateProjectionMatrix();    
 }
 
 
